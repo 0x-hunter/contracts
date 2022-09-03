e6f8a3e22e927e5f08bdc40e6247814ca3214247
0x50fc <0x50Fc@gmail.com>
2022-09-03 12:12:31 +0800

    up

--- ci/abi.sh
+++ ci/abi.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-abi-types-generator abi/Raffle.json --output ts/src
-abi-types-generator abi/MockERC20.json --output ts/src
-abi-types-generator abi/MockERC721.json --output ts/src
+cd abi
+for ITEM in $(ls *.json)
+do
+    abi-types-generator $ITEM --output ../ts/src --provider=web3
+    ../ci/abi_copy.js $ITEM ../ts/src
+done
+
+exit 0
+
+abi-types-generator abi/Raffle.json --output ts/src --provider=web3
+abi-types-generator abi/MockERC20.json --output ts/src --provider=web3
+abi-types-generator abi/MockERC721.json --output ts/src --provider=web3
