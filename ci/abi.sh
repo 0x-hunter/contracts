91c418e87c5921741759c9411e28839bf7c43fb5
0x50fc <0x50Fc@gmail.com>
2022-09-03 17:05:59 +0800

    add web3

--- ci/abi.sh
+++ ci/abi.sh
@@ -4,11 +4,5 @@ cd abi
 for ITEM in $(ls *.json)
 do
     abi-types-generator $ITEM --output ../ts/src --provider=web3
-    ../ci/abi_copy.js $ITEM ../ts/src
+    ../ci/abi_web3.js $ITEM ../ts/src
 done
-
-exit 0
-
-abi-types-generator abi/Raffle.json --output ts/src --provider=web3
-abi-types-generator abi/MockERC20.json --output ts/src --provider=web3
-abi-types-generator abi/MockERC721.json --output ts/src --provider=web3
